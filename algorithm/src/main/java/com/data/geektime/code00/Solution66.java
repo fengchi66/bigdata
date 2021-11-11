24f94e71b3406d35efe1f9206824514a5bb52d61
wufengchi <wufengchi@yit.com>
2021-11-11 14:20:55 +0800

    init

---
+++ algorithm/src/main/java/com/data/geektime/code00/Solution66.java
@@ -0,0 +1,26 @@
+package com.data.geektime.code00;
+
+// 加一
+public class Solution66 {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i--) {
+            // 判断加1后是不是0
+            digits[i] = (digits[i] + 1) % 10;
+            if (digits[i] != 0) {
+                return digits;
+            }
+        }
+        // 加一后都是0
+        digits = new int[len + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        nums = new int[4];
+        nums[0] = 1;
+    }
+}
+
