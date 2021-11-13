7ff8b873cf4b1af747be904b31a850a717384539
wufengchi <wufengchi@yit.com>
2021-11-13 15:37:31 +0800

    合并两个有序数组

---
+++ algorithm/src/main/java/com/data/geektime/code01/Solution88.java
@@ -0,0 +1,30 @@
+package com.data.geektime.code01;
+
+// 合并两个有序数组
+public class Solution88 {
+
+  // 其实就是归并排序的merge过程
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int[] nums = new int[m + n];
+    int index = 0;
+    int i = 0;
+    int j = 0;
+
+    while (i < m && j < n) {
+      nums[index++] = nums1[i] <= nums2[j] ? nums1[i++] : nums2[j++];
+    }
+
+    // num1或者num2没有遍历完
+    while (i < m) {
+      nums[index++] = nums1[i++];
+    }
+    while (j < n) {
+      nums[index++] = nums2[j++];
+    }
+
+    // 把nums赋值给nums
+    for (int k = 0; k < nums.length; k++) {
+      nums1[k] = nums[k];
+    }
+  }
+}
