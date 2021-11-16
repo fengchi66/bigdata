81434294541ad192bc597c6e8314924f78675e87
wufengchi <wufengchi@yit.com>
2021-11-16 19:02:32 +0800

    BinarySearch

---
+++ algorithm/src/main/java/com/data/offer02/demo/BinarySearch.java
@@ -0,0 +1,23 @@
+package com.data.offer02.demo;
+
+public class BinarySearch {
+
+  public int search(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
+      if (nums[mid] > target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return -1;
+  }
+
+}
