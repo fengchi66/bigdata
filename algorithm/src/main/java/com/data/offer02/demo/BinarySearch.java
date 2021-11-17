d85f73140c34b4c5339ec11967cb718689efa155
wufengchi <wufengchi@yit.com>
2021-11-17 15:16:35 +0800

    二分查找变体

--- algorithm/src/main/java/com/data/offer02/demo/BinarySearch.java
+++ algorithm/src/main/java/com/data/offer02/demo/BinarySearch.java
@@ -87,7 +87,36 @@ public class BinarySearch {
   }
 
   // 二分变体3：查找第一个大于等于给定值的元素
+  public int search5(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (nums[mid] >= target) { // 此时位于mid的元素>=target，判断mid是不是第一个元素
+        if (mid == 0 || nums[mid - 1] < target) { // 是第一个元素，返回
+          return mid;
+        }
+      } else {
+        left = mid + 1;
+      }
+    }
+    return -1;
+  }
 
-
-
+  // 二分变体4：查找最后一个小于等于给定值的元素
+  public int search6(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (nums[mid] <= target) { // 此时num[mid] <= target，判断是不是最后一个
+        if (mid == nums.length - 1 || nums[mid + 1] > target) { // 是最后一个
+          return mid;
+        }
+      } else {
+        left = mid + 1;
+      }
+    }
+    return -1;
+  }
 }
