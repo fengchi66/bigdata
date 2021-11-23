b35d550c88402eb73b1454dd31cefb409fd4064e
wufengchi <wufengchi@yit.com>
2021-11-23 14:16:31 +0800

    包含k个元素的组合

--- algorithm/src/main/java/com/data/offer02/code/Solution80.java
+++ algorithm/src/main/java/com/data/offer02/code/Solution80.java
@@ -1,13 +1,32 @@
 package com.data.offer02.code;
 
+import java.util.LinkedList;
 import java.util.List;
 
-// 含有 k 个元素的组合
+// 包含k个元素的组合
 public class Solution80 {
 
   public List<List<Integer>> combine(int n, int k) {
-    return null;
+    List<List<Integer>> ans = new LinkedList<>();
+    LinkedList<Integer> subset = new LinkedList<>();
 
+    // 在数字[1...n]中选出k和数，从1开始
+    dfs(n, k, 1, ans, subset);
+    return ans;
   }
 
+  private void dfs(int n, int k, int index, List<List<Integer>> ans, LinkedList<Integer> subset) {
+    // base case
+    if (subset.size() == k) {
+      ans.add(new LinkedList<>(subset));
+    } else if (index <= n) {
+      // 不要当前元素
+      dfs(n, k, index + 1, ans, subset);
+
+      // 要当前元素
+      subset.add(index);
+      dfs(n, k, index + 1, ans, subset);
+      subset.removeLast();
+    }
+  }
 }
---
+++ algorithm/src/main/java/com/data/offer02/code/Solution81.java
@@ -0,0 +1,6 @@
+package com.data.offer02.code;
+
+// 允许重复选择元素的组合
+public class Solution81 {
+
+}
