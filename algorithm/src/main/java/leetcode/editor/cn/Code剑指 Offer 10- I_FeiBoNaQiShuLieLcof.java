a51b23bbb2fdb451e324a8a5e046a1bb9bd77abd
wufengchi <wufengchi@yit.com>
2021-12-02 18:07:53 +0800

    init

--- algorithm/src/main/java/leetcode/editor/cn/Code剑指 Offer 10- I_FeiBoNaQiShuLieLcof.java
+++ algorithm/src/main/java/leetcode/editor/cn/Code剑指 Offer 10- I_FeiBoNaQiShuLieLcof.java
@@ -42,6 +42,17 @@ class Code_Offer_FeiBoNaQiShuLieLcof {
   class Solution {
 
     public int fib(int n) {
+      final int MOD = 1000000007;
+      if (n < 2) {
+        return n;
+      }
+      int p = 0, q = 0, r = 1;
+      for (int i = 2; i <= n; ++i) {
+        p = q;
+        q = r;
+        r = (p + q) % MOD;
+      }
+      return r;
 
     }
   }
