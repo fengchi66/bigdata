0126c4ef009d1655b766747340f6498835401610
wufengchi <wufengchi@yit.com>
2022-02-07 22:12:41 +0800

    init

--- algorithm/src/main/java/leetcode/editor/cn/Code208_ImplementTriePrefixTree.java
+++ algorithm/src/main/java/leetcode/editor/cn/Code208_ImplementTriePrefixTree.java
@@ -46,41 +46,67 @@
 
 package leetcode.editor.cn;
 
-// 208.实现 Trie (前缀树)
+// 208.实现 Trie (前缀树):以下三种操作的时间复杂度都是O(n)
 public class Code208_ImplementTriePrefixTree {
 
   //leetcode submit region begin(Prohibit modification and deletion)
+  // 实现 Trie (前缀树):以下三种操作的时间复杂度都是O(n)
   class Trie {
 
-    class Node {
+    class TrieNode {
 
-      int count;
-      Node[] child;
+      TrieNode child[]; // 前缀树的节点可能有26个子节点
+      boolean isWorld; // 到达该节点的路径对应的字符串是否为字典中的完整单词
 
-      Node() {
-        count = 0;
-        child = new Node[26];
+      TrieNode() {
+        child = new TrieNode[26];
       }
     }
 
-    public Trie() {
+    private TrieNode root;
 
+    public Trie() {
+      root = new TrieNode();
     }
 
     public void insert(String word) {
-      find(word, true, false);
+      // 1. 首先前往前缀树的根节点
+      TrieNode node = root;
+      // 2. 确定该根节点是否存在一个子节点与第一个字符对应，如果存在则前往，如果不存在则添加后前往
+      for (char ch : word.toCharArray()) {
+        if (node.child[ch - 'a'] == null) {
+          node.child[ch - 'a'] = new TrieNode();
+        }
+        // 3. 以此类推将字符串中的其他字符添加到前缀树中
+        node = node.child[ch - 'a'];
+      }
+      // 4. 全部添加进去后，将最后一个节点的标识设置为true
+      node.isWorld = true;
     }
 
+    // 思路和insert类似
     public boolean search(String word) {
-      return find(word, false, false);
+      // 1. 从根节点开始查找，如果子节点没有与字符相等，返回false，否则继续往下查找，直到最后一个字符
+      TrieNode node = root;
+      for (char ch : word.toCharArray()) {
+        if (node.child[ch - 'a'] == null) {
+          return false;
+        }
+        node = node.child[ch - 'a'];
+      }
+      return node.isWorld;
     }
 
     public boolean startsWith(String prefix) {
-      return find(prefix, false, true);
-    }
-
-    private boolean find(String s, boolean isInsert, boolean isPrefix) {
-
+      TrieNode node = root;
+      for (char ch : prefix.toCharArray()) {
+        if (node.child[ch - 'a'] == null) {
+          return false;
+        }
+        node = node.child[ch - 'a'];
+      }
+      // 能查完prefix的全部字符，说明一定startsWith
+      return true;
     }
   }
 
