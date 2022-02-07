163656873e8664e8189dd8e1703f1e3f134b22b2
wufengchi <wufengchi@yit.com>
2022-02-07 17:25:06 +0800

    init

---
+++ algorithm/src/main/java/leetcode/editor/cn/Code208_ImplementTriePrefixTree.java
@@ -0,0 +1,96 @@
+//Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼
+//写检查。 
+//
+// 请你实现 Trie 类： 
+//
+// 
+// Trie() 初始化前缀树对象。 
+// void insert(String word) 向前缀树中插入字符串 word 。 
+// boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 
+//false 。 
+// boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否
+//则，返回 false 。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入
+//["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+//[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+//输出
+//[null, null, true, false, true, null, true]
+//
+//解释
+//Trie trie = new Trie();
+//trie.insert("apple");
+//trie.search("apple");   // 返回 True
+//trie.search("app");     // 返回 False
+//trie.startsWith("app"); // 返回 True
+//trie.insert("app");
+//trie.search("app");     // 返回 True
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= word.length, prefix.length <= 2000 
+// word 和 prefix 仅由小写英文字母组成 
+// insert、search 和 startsWith 调用次数 总计 不超过 3 * 10⁴ 次 
+// 
+// Related Topics 设计 字典树 哈希表 字符串 👍 1049 👎 0
+
+package leetcode.editor.cn;
+
+// 208.实现 Trie (前缀树)
+public class Code208_ImplementTriePrefixTree {
+
+  //leetcode submit region begin(Prohibit modification and deletion)
+  class Trie {
+
+    class Node {
+
+      int count;
+      Node[] child;
+
+      Node() {
+        count = 0;
+        child = new Node[26];
+      }
+    }
+
+    public Trie() {
+
+    }
+
+    public void insert(String word) {
+      find(word, true, false);
+    }
+
+    public boolean search(String word) {
+      return find(word, false, false);
+    }
+
+    public boolean startsWith(String prefix) {
+      return find(prefix, false, true);
+    }
+
+    private boolean find(String s, boolean isInsert, boolean isPrefix) {
+
+    }
+  }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ algorithm/src/main/java/leetcode/editor/cn/doc/content/Code208_ImplementTriePrefixTree.md
@@ -0,0 +1,42 @@
+<p><strong><a href="https://baike.baidu.com/item/字典树/9825209?fr=aladdin" target="_blank">Trie</a></strong>（发音类似 "try"）或者说 <strong>前缀树</strong> 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。</p>
+
+<p>请你实现 Trie 类：</p>
+
+<ul>
+	<li><code>Trie()</code> 初始化前缀树对象。</li>
+	<li><code>void insert(String word)</code> 向前缀树中插入字符串 <code>word</code> 。</li>
+	<li><code>boolean search(String word)</code> 如果字符串 <code>word</code> 在前缀树中，返回 <code>true</code>（即，在检索之前已经插入）；否则，返回 <code>false</code> 。</li>
+	<li><code>boolean startsWith(String prefix)</code> 如果之前已经插入的字符串 <code>word</code> 的前缀之一为 <code>prefix</code> ，返回 <code>true</code> ；否则，返回 <code>false</code> 。</li>
+</ul>
+
+<p> </p>
+
+<p><strong>示例：</strong></p>
+
+<pre>
+<strong>输入</strong>
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+<strong>输出</strong>
+[null, null, true, false, true, null, true]
+
+<strong>解释</strong>
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // 返回 True
+trie.search("app");     // 返回 False
+trie.startsWith("app"); // 返回 True
+trie.insert("app");
+trie.search("app");     // 返回 True
+</pre>
+
+<p> </p>
+
+<p><strong>提示：</strong></p>
+
+<ul>
+	<li><code>1 <= word.length, prefix.length <= 2000</code></li>
+	<li><code>word</code> 和 <code>prefix</code> 仅由小写英文字母组成</li>
+	<li><code>insert</code>、<code>search</code> 和 <code>startsWith</code> 调用次数 <strong>总计</strong> 不超过 <code>3 * 10<sup>4</sup></code> 次</li>
+</ul>
+<div><div>Related Topics</div><div><li>设计</li><li>字典树</li><li>哈希表</li><li>字符串</li></div></div><br><div><li>👍 1049</li><li>👎 0</li></div>
