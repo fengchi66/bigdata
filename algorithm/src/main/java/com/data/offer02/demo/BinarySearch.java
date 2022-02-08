85e706543f56318603c4966fa3aac2b253c1e6d9
wufengchi <wufengchi@yit.com>
2022-02-08 15:35:20 +0800

    init

--- algorithm/src/main/java/com/data/offer02/demo/BinarySearch.java
+++ algorithm/src/main/java/com/data/offer02/demo/BinarySearch.java
@@ -91,10 +91,13 @@ public class BinarySearch {
     int low = 0;
     int high = n - 1;
     while (low <= high) {
-      int mid =  low + ((high - low) >> 1);
+      int mid = low + ((high - low) >> 1);
       if (a[mid] >= value) {
-        if ((mid == 0) || (a[mid - 1] < value)) return mid;
-        else high = mid - 1;
+        if ((mid == 0) || (a[mid - 1] < value)) {
+          return mid;
+        } else {
+          high = mid - 1;
+        }
       } else {
         low = mid + 1;
       }
@@ -107,12 +110,15 @@ public class BinarySearch {
     int low = 0;
     int high = n - 1;
     while (low <= high) {
-      int mid =  low + ((high - low) >> 1);
+      int mid = low + ((high - low) >> 1);
       if (a[mid] > value) {
         high = mid - 1;
       } else {
-        if ((mid == n - 1) || (a[mid + 1] > value)) return mid;
-        else low = mid + 1;
+        if ((mid == n - 1) || (a[mid + 1] > value)) {
+          return mid;
+        } else {
+          low = mid + 1;
+        }
       }
     }
     return -1;
---
+++ algorithm/src/main/java/leetcode/editor/cn/Code704_BinarySearch.java
@@ -0,0 +1,53 @@
+//给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否
+//则返回 -1。 
+//
+// 
+//示例 1: 
+//
+// 输入: nums = [-1,0,3,5,9,12], target = 9
+//输出: 4
+//解释: 9 出现在 nums 中并且下标为 4
+// 
+//
+// 示例 2: 
+//
+// 输入: nums = [-1,0,3,5,9,12], target = 2
+//输出: -1
+//解释: 2 不存在 nums 中因此返回 -1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 你可以假设 nums 中的所有元素是不重复的。 
+// n 将在 [1, 10000]之间。 
+// nums 的每个元素都将在 [-9999, 9999]之间。 
+// 
+// Related Topics 数组 二分查找 👍 621 👎 0
+
+package leetcode.editor.cn;
+
+// 704.二分查找
+public class Code704_BinarySearch {
+
+  //leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+
+    public int search(int[] nums, int target) {
+      int left = 0;
+      int right = nums.length - 1;
+
+      while (left <= right) {
+        int mid = (left + right) / 2;
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target) left = mid +1;
+        else right = mid-1;
+      }
+      return -1;
+    }
+  }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
